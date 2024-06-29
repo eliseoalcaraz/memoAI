77e54da83120bdfeffa44cecffc7dd6154facb13
Eliseo Alcaraz <elipalcaraz@gmail.com>
2024-06-29 19:11:06 +0800

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-//import { applyMiddleware, compose } from 'redux';
-import { thunk } from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import reducers from './reducers';
-
 import App from './App';
 import './index.css';
 
-import { configureStore } from '@reduxjs/toolkit';
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
-const store = configureStore({
-    reducer: reducers,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
-});
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
 );
